c029a3ea1661c617cd79416b4419d6a660ea65f7
sharifok1 <sharifok1@gmail.com>
2022-05-31 13:02:45 +0600

    add real video

--- src/Component/LandingPage/HomePage/HomePage.jsx
+++ src/Component/LandingPage/HomePage/HomePage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useReducer } from 'react';
 import './HomePage.css'
 import banner from '../../assets/homeBaner.png'
 import { Container } from 'react-bootstrap';
@@ -7,20 +7,31 @@ import demovideo from '../../assets/demo.mp4';
 import videoTham from '../../assets/videoTham.png'
 
 const HomePage = () => {
+    const videoEl = useReducer(null);
 
-    const [open, setOpen] = React.useState(false);
-    const handleOpen = () => setOpen(true);
-    const handleClose = () => setOpen(false);
-    const style = {
-        position: 'absolute',
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)',
-        width:'auto',
-        bgcolor: 'transfarent',
-        boxShadow: 24,
-        p: 4,
-      };
+    const attemptPlay = () => {
+      videoEl &&
+        videoEl.current &&
+        videoEl.current.play().catch((error) => {
+          console.error("Error attempting to play", error);
+        });
+    };
+    useEffect(() => {
+        attemptPlay();
+      }, []);
+    // const [open, setOpen] = React.useState(false);
+    // const handleOpen = () => setOpen(true);
+    // const handleClose = () => setOpen(false);
+    // const style = {
+    //     position: 'absolute',
+    //     top: '50%',
+    //     left: '50%',
+    //     transform: 'translate(-50%, -50%)',
+    //     width:'auto',
+    //     bgcolor: 'transfarent',
+    //     boxShadow: 24,
+    //     p: 4,
+    //   };
 
     return (
         <div id='home'>
@@ -30,16 +41,27 @@ const HomePage = () => {
                 <h1 className='mainHeading'>
                     <span className='text-yellow'>It’s Time</span> to Transform Your <span className='text-yellow'>Dream Life</span> into <span>Reality</span></h1>
             </div>
-            {/* <div className='banner-pair-btn'> */}
-                <div className='video-wraper effect-perple effect-blue'>
-                    <div className='modal-video'>
-                    <img className='img-fluid' src={videoTham} alt="" />
-                    <button onClick={()=>handleOpen()} className='play-thevideo-btn'><i class="fa-solid fa-play play-icon"></i></button>
+                <div className='video-wraper effect-perple effect-blue p-md-5'>
+                    <div className='modal-video p-md-5 mx-md-5 my-5 my-md-0 pb-5'>
+                    {/* <img className='img-fluid' src={videoTham} alt="" /> */}
+                    {/* <button onClick={()=>handleOpen()} className='play-thevideo-btn'><i class="fa-solid fa-play play-icon"></i></button> */}
+
+                    {/* <Box sx={style}> */}
+                                <video
+                                 style={{ maxWidth: "100%", width: "800px", margin: "0 auto" }}
+                                 playsInline
+                                 loop
+                                 controls
+                                 alt="All the devices"
+                                 src={demovideo}
+                                 ref={videoEl}
+                               />
+                                {/* </Box> */}
                     </div>
                 </div>
                 
                  
-                              <Modal      
+                              {/* <Modal      
                                 aria-labelledby="transition-modal-title"
                                 aria-describedby="transition-modal-description"
                                 open={open}
@@ -49,19 +71,11 @@ const HomePage = () => {
                                 BackdropProps={{
                                 timeout: 500,
                                 }}
-                            >
-                                <Fade in={open}>
-                                <Box sx={style}>
-                                <video
-                                style={{width:'100%'}}
-                                    autoPlay
-                                    controls
-                                >
-                                    <source src={demovideo} type="video/mp4"/>
-                                    </video>
-                                </Box>
-                                </Fade>
-                            </Modal>
+                            > */}
+                                {/* <Fade in={open}> */}
+                               
+                                {/* </Fade> */}
+                            {/* </Modal> */}
              {/* </div> */}
              <div className='primary-text px-5'>
                  <p className='px-md-5 '>
